afc8a71e402593d2e272bcd0646a059473930cc5
Aaramis <auguste.gardette@hotmail.fr>
2022-09-18 17:30:26 +0200

    checker functionnal but not clean

--- SRCS/bonus.c
+++ SRCS/bonus.c
@@ -6,24 +6,22 @@
 
 int	ft_nv_b3(t_stack **stack_a, t_stack **stack_b, char *str)
 {
-	if (!ft_strncmp(str, "rrr", 2))
+	if (!ft_strncmp(str, "rrr", 3))
 	{
 		ft_rr(stack_a, 0);
-		ft_rr(stack_b, 3);
+		ft_rr(stack_b, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "rr", 3))
+	else if (!ft_strncmp(str, "rra", 3))
 	{
-		ft_r(stack_a, 0);
-		ft_r(stack_b, 3);
+		ft_rr(stack_a, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "ss", 3))
+	else if (!ft_strncmp(str, "rrb", 3))
 	{
-		ft_s(stack_a, 0);
-		ft_s(stack_b, 3);
+		ft_rr(stack_b, 0);
 		return (1);
-	}	
+	}
 	return (0);
 }
 
@@ -31,24 +29,26 @@ int	ft_nv_b2(t_stack **stack_a, t_stack **stack_b, char *str)
 {
 	if (!ft_strncmp(str, "ra", 2))
 	{
-		ft_r(stack_a, 1);
+		ft_r(stack_a, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "rb", 2))
+	else if (!ft_strncmp(str, "rb", 2))
 	{
-		ft_r(stack_b, 2);
+		ft_r(stack_b, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "rra", 3))
+	else if (!ft_strncmp(str, "rr", 2))
 	{
-		ft_rr(stack_a, 1);
+		ft_r(stack_a, 0);
+		ft_r(stack_b, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "rrb", 3))
+	else if (!ft_strncmp(str, "ss", 2))
 	{
-		ft_rr(stack_b, 2);
+		ft_s(stack_a, 0);
+		ft_s(stack_b, 0);
 		return (1);
-	}
+	}	
 	return (0);
 }
 
@@ -56,22 +56,22 @@ int	ft_nv_b1(t_stack **stack_a, t_stack **stack_b, char *str)
 {
 	if (!ft_strncmp(str, "pa", 2))
 	{
-		ft_p(stack_a, stack_b, 1);
+		ft_p(stack_b, stack_a, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "pb", 2))
+	else if (!ft_strncmp(str, "pb", 2))
 	{
-		ft_p(stack_b, stack_a, 2);
+		ft_p(stack_a, stack_b, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "sa", 2))
+	else if (!ft_strncmp(str, "sa", 2))
 	{
-		ft_s(stack_a, 1);
+		ft_s(stack_a, 0);
 		return (1);
 	}
-	if (!ft_strncmp(str, "sb", 2))
+	else if (!ft_strncmp(str, "sb", 2))
 	{
-		ft_s(stack_b, 2);
+		ft_s(stack_b, 0);
 		return (1);
 	}
 	return (0);
@@ -83,12 +83,16 @@ void	ft_checker_str(t_stack **stack_a, t_stack **stack_b, char *str)
 	int	i;
 
 	i = 0;
-	i += ft_nv_b1(stack_a, stack_b, str);
-	i += ft_nv_b2(stack_a, stack_b, str);
-	i += ft_nv_b3(stack_a, stack_b, str);
+	if (ft_strlen(str) == 3)
+	{
+		i += ft_nv_b1(stack_a, stack_b, str);
+		i += ft_nv_b2(stack_a, stack_b, str);
+	}
+	else
+		i += ft_nv_b3(stack_a, stack_b, str);
 	if (!i)
 	{
-		i = write(1, "Error\n", 6);
+		i = write(2, "Error\n", 6);
 		(void)i;
 		exit (1);
 	}
@@ -98,6 +102,7 @@ void	ft_lst_check_b(t_stack **begin)
 {
 	t_stack	*next;
 	t_stack	*first;
+	int		i;
 
 	first = (*begin);
 	while (first && first->next)
@@ -107,6 +112,13 @@ void	ft_lst_check_b(t_stack **begin)
 			break ;
 		first = next;
 	}
+	if (!first->next)
+		i = write(1,"OK\n", 3);
+	else
+		i = write(1,"KO\n", 3);
+	(void)i;
+	ft_lstclear_s(begin);
+	exit (0);
 }
 
 int	main(int argc, char **argv)
@@ -125,20 +137,19 @@ int	main(int argc, char **argv)
 		else
 			stack_a = ft_parsing2(argv[1]);
 	}
-	while (get_next_line(0, &str) == 1)
+	while (get_next_line(0, &str) == 1 && str)
 	{
-		ft_printf("%s\n", str);
 		ft_checker_str(&stack_a, &stack_b, str);
-		ft_print_ma_list(&stack_a);
+	//	ft_print_ma_list(&stack_a);
+	//	ft_printf("\n");
 		free(str);
 	}
 	(void)i;
 	(void)str;
-	ft_lst_check_sort(&stack_a);
-	ft_print_ma_list(&stack_a);
-	ft_printf("\n");
-	ft_print_ma_list(&stack_b);
-	ft_lstclear_s(&stack_a);
+//	ft_print_ma_list(&stack_a);
 	ft_lstclear_s(&stack_b);
+	ft_lst_check_b(&stack_a);
+//	ft_printf("\n");
+//	ft_print_ma_list(&stack_b);
 	return (0);
 }
